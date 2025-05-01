5f342a34a80f601d77a081d72882cf2661927749
Zack Amiton <zamiton@outlook.com>
2025-05-01 14:39:41 -0400

    swag on the haters

--- con-visualizer.js
+++ con-visualizer.js
@@ -1,72 +1,3 @@
-// // ⚠️ CONFIGURE THESE PARAMETERS BEFORE HITTING RUN IN STERLING ⚠️ 
-// // The behavior of setInterval means that hitting run multiple times will orphan the 
-// // interval (which is bound to the window itself) being used to visualize traces,
-// // which will result in buggy behavior!
-// const LOOP = true;
-// const TIME_STEP = 1000;
-
-// let globalIterations = 0;
-// const gridSize = 16;
-
-// const offsetHelper = (i) => i + 8;
-
-// let activeInterval = setInterval(() => {
-//     const stage = new Stage();
-
-//     const activeIteration = instances[globalIterations];
-
-//     let grid = new Grid({
-//         grid_location: {x: 50, y: 50},
-//         cell_size: {x_size: 35, y_size: 35},
-//         grid_dimensions: {x_size: gridSize, y_size: gridSize}
-//     });
-
-//     for(let f of activeIteration._fields) {
-//         switch(f._id) {
-//             case "infected":
-//                 for(let i = 0; i < f._tuples.length; i++) {
-//                     const row = offsetHelper(parseInt(f._tuples[i]._atoms[1]._id));
-//                     const col = offsetHelper(parseInt(f._tuples[i]._atoms[2]._id));
-//                     grid.add({x: row, y: col}, new Circle({radius: 10, color: "red"}));
-//                 }
-//                 break;
-//             case "susceptible":
-//                 for(let i = 0; i < f._tuples.length; i++) {
-//                     const row = offsetHelper(parseInt(f._tuples[i]._atoms[1]._id));
-//                     const col = offsetHelper(parseInt(f._tuples[i]._atoms[2]._id));
-//                     grid.add({x: row, y: col}, new Circle({radius: 8, color: "blue"}));
-//                 }
-//                 break;
-//             case "recovered":
-//                 for(let i = 0; i < f._tuples.length; i++) {
-//                     const row = offsetHelper(parseInt(f._tuples[i]._atoms[1]._id));
-//                     const col = offsetHelper(parseInt(f._tuples[i]._atoms[2]._id));
-//                     grid.add({x: row, y: col}, new Circle({radius: 5, color: "green"}));
-//                 }
-//                 break;
-//             default:
-//                 break;
-
-//         }
-//     }
-
-//     stage.add(grid);
-//     stage.add(new TextBox({
-//         text: `Timestep ${globalIterations + 1} / ${instances.length} [${LOOP ? 'Looping' : 'Linear'}]`,
-//         coords: {x: 250, y: 20},
-//         color: 'black',
-//         fontSize: 16
-//     }))
-
-//     stage.render(svg, document);
-//     globalIterations = (globalIterations + 1) % instances.length;
-
-//     // Just to prevent a memory leak, for now!
-//     if(globalIterations == 0 && !LOOP) {
-//         clearInterval(activeInterval);
-//     }
-// }, TIME_STEP);
-
 // ────── CONSTANTS ────────────────────────────────────────────────
 
 const GRID_SIZE = 16;      
@@ -75,13 +6,18 @@ const GRID_XY = {x: 50, y: 50};
 const STATE_COL = {
     susceptible: "#8E8E8E",
     infected: "#FF4500",
-    recovered: "#00C96B"};
+    recovered: "#00C96B"
+};
 
-const BUTTONS = ["PREV", "NEXT"];
+const BUTTONS = {
+  "PREV": prevStep,
+  "NEXT": nextStep,
+  "RESET": reset
+}
 const BTN_W = 80;
 const BTN_H = 30;
 const BTN_Y = GRID_XY.y + GRID_SIZE * CELL_SIZE + 20;              
-const BTN_X0 = (GRID_XY.x + GRID_SIZE * CELL_SIZE)/2 - 60;                
+const BTN_X0 = (GRID_XY.x + GRID_SIZE * CELL_SIZE)/2 - 90;                
 // -------------------------------------------------------------------
 
 // Offsets the Forge Int atom ids (-8 to 7) to 0 to 15 indices
@@ -143,6 +79,7 @@ function render() {
 
 function prevStep() { if (index > 0) { index--; render(); } }
 function nextStep() { if (index < maxT-1) { index++; render(); } }
+function reset() { index = 0; render(); }
 
 // ---------------- SET UP -------------------------------------------
 statusBox = new TextBox({
@@ -154,7 +91,7 @@ statusBox = new TextBox({
 stage.add(statusBox);
 
 // create PREV / NEXT buttons
-BUTTONS.forEach((label, i) => {
+Object.keys(BUTTONS).forEach((label, i) => {
   const x0 = BTN_X0 + i*(BTN_W + 10);
 
   stage.add(new Rectangle({
@@ -163,6 +100,7 @@ BUTTONS.forEach((label, i) => {
     height : BTN_H,
     color  : '#cccccc'
   }));
+
   stage.add(new TextBox({
     text     : label,
     coords   : {x: x0 + BTN_W/2, y: BTN_Y + BTN_H/2},
@@ -170,9 +108,7 @@ BUTTONS.forEach((label, i) => {
     fontSize : 14,
     events   : [{
       event    : 'click',
-      callback : () => {
-        (label === "PREV" ? prevStep() : nextStep());
-      }
+      callback : BUTTONS[label]
     }]
   }));
 });
